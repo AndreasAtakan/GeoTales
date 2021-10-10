78ed5bd87dcef0217c2af8543efebae3d00ec4f9
AndreasAtakan <andreas@napkin.no>
2021-10-10 16:05:29 +0200

    outline work

--- v1/src/js/main.js
+++ v1/src/js/main.js
@@ -4,6 +4,7 @@ window.onload = function(ev) {
 	let map = L.map("map", {
 		center: [26.58852714730864, -6.152343750000001],
 		zoom: 3,
+		zoomControl: false,
 		maxZoom: 18
 	});
 
@@ -11,4 +12,7 @@ window.onload = function(ev) {
 		attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OSM</a>"
 	});
 	map.addLayer(OSM);
-}
+
+
+	$("div.leaflet-control-attribution a").attr("target", "_blank");
+};
