1e46e1bba29fff3b13622e95c8eec6e1f19da073
AndreasAtakan <andreas@napkin.no>
2021-10-15 17:31:46 +0200

    Added map timeline

--- v1/src/js/main.js
+++ v1/src/js/main.js
@@ -2,10 +2,16 @@
 
 window.onload = function(ev) {
 	let map = L.map("map", {
-		center: [26.58852714730864, -6.152343750000001],
-		zoom: 3,
+		center: [ 49.49667452747045, 12.128906250000002 ],
+		zoom: window.innerWidth < 575.98 ? 3 : 4,
 		zoomControl: false,
-		maxZoom: 18
+		maxZoom: 18,
+		timeDimension: true,
+		timeDimensionOptions: {
+			timeInterval: "P1D/2021-10-15",
+			period: "PT5M"
+		},
+		//timeDimensionControl: true
 	});
 
 	/*map.addControl(
@@ -22,6 +28,22 @@ window.onload = function(ev) {
 
 	map.addControl(
 		new L.Control.Fullscreen({ position: "topright" })
+	);
+
+	map.addControl(
+		L.control.timeDimension({
+			position: "bottomleft",
+			loopButton: true,
+			//limitSliders: true, // TODO: fix width styling for this
+			speedSlider: false,
+			//minSpeed: 1,
+			//maxSpeed: 5,
+			//speedStep: 1,
+			timeZones: [ "Local" ],
+			playerOptions: {
+				startOver: true
+			}
+		})
 	);
 
 	let OSM = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
